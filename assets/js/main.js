0f3b41099b2ed28f6d2ec915b13f7f811b879781
codewurld <sholaq@gmail.com>
2021-09-28 14:52:06 +0100

    added portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,23 @@ const toggleSkills = () => {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+// Qualification Functions
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_Active')
+
+        [tab].forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
